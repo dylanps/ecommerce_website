02d834a9298211fa301aee5f3f96f94820d24a5a
Dylan Sellers <dylan.sellers@hotmail.com>
2019-05-24 09:07:37 -0600

    Initial site upload

---
+++ Main.js
@@ -0,0 +1,15 @@
+const navlink = document.getElementsByClassName('nav-link');
+
+navlink[0].addEventListener('hovermouseover', (e) => {
+
+    console.log(navlink[0]);
+    navlink[0].setAttribute
+
+});
+
+//function that dynamically adds the product to the users cart.
+function addtoCart(params) {
+    
+}
+
+
