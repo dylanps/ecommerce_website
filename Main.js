eeca849b40c42cb2d27f7fc089983fe53225097a
Dylan Sellers <dylan.sellers@hotmail.com>
2019-05-29 09:55:19 -0600

    Update Main.js
    Product and CartManager Classes added

--- Main.js
+++ Main.js
@@ -1,15 +1,53 @@
-const navlink = document.getElementsByClassName('nav-link');
+// const navlink = document.getElementsByClassName('nav-link');
 
-navlink[0].addEventListener('hovermouseover', (e) => {
+// navlink[0].addEventListener('hovermouseover', (e) => {
 
-    console.log(navlink[0]);
-    navlink[0].setAttribute
+//     console.log(navlink[0]);
+//     navlink[0].setAttribute
 
-});
+// });
 
 //function that dynamically adds the product to the users cart.
-function addtoCart(params) {
+
+//all cart 
+class CartManager {
+    constructor() {
+        
+    }
+
+    // cart = [];
+
+    addtoCart() {
+        // this.cart
+    }
+    removefromCart() {
+
+    }
+    emptyCart() {
+        //while cart is not empty, remove items (or delete cart?)
+    }
+}
+
+console.log(CartManager);
+
+class Product {
+    constructor(id, label, price) {
+        
+        //input parameters
+        this.id = id;
+        this.label = label;
+        this.price = price;
+
+        //other parameters
+        this.quantity = 0;
+        this.favorites = 0;
+        this.reviews = 0;
+        this.comments = [];
+    }
     
 }
 
+const myBelt = new Product(1, 'belt', 10.00);
+
+console.log(myBelt);
 
